05b9df7e4c29fd398b346f85b88c332c4dddfba3
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2024-11-19 20:29:55 -0500

    data relationships

---
+++ src/test/java/pe/joedayz/database_relationships/DatabaseRelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.joedayz.database_relationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseRelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
